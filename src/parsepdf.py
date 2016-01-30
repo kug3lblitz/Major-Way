e966b6be1cd05103f6d03fc6938381c139fb3869
Jaime5 <Jaime5@UMBC.edu>
2016-01-30 11:34:42 -0500

    rawr

--- src/parsepdf.py
+++ src/parsepdf.py
@@ -47,21 +47,21 @@ Input: None
 Output: Textfile of parsed data
 '''
 
-
-def test():
-    from os.path import abspath, dirname, join
-
-    BASEDIR = dirname(abspath(__file__))
-    path = join(BASEDIR, "samples", raw_input("Enter file path: "))
-
-    try:
-        words = getPDFText(str(path) + ".pdf")
-        if words != '':
-            with open("samples/parsed.txt", "wb") as result:
-                result.write(words)
-            print "Text is extracted and saved as 'parsed.txt'."
-
-    except IOError:
-        exit("Invalid file name.")
+#
+# def test():
+#     from os.path import abspath, dirname, join
+#
+#     BASEDIR = dirname(abspath(__file__))
+#     path = join(BASEDIR, "samples", raw_input("Enter file path: "))
+#
+#     try:
+#         words = getPDFText(str(path) + ".pdf")
+#         if words != '':
+#             with open("samples/parsed.txt", "wb") as result:
+#                 result.write(words)
+#             print "Text is extracted and saved as 'parsed.txt'."
+#
+#     except IOError:
+#         exit("Invalid file name.")
 
 # test()
